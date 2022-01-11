674c4a850a884e87e951a68b9424cac2c1c38b4f
ranga27 <sarang.radke@gmail.com>
2022-01-11 18:21:50 +0000

    changes 2022

--- README.md
+++ README.md
@@ -1,21 +1,24 @@
 # loop-not-luck
 
 ## TODO
+
+- upload image has a bug, clear input field
+- opps data saved as nested object
 - Forgot password route
 - Create user route - refactor app.js into public & private routes
-- Design same approach for components 
+- Design same approach for components
 - Local storage vs realtime storage
 - Add index.js to all folders for better export
 - Use security rules in firestore to restrict webpages & test
 - Cleanup unnecessary files
-- Add machine learning to get candidate and opportunity search 
+- Add machine learning to get candidate and opportunity search
 - local emulator for functions is not working may be due to firestore & auth not running locally
 - 1st screen for companies is roles tiles & then clicking on it should go to individual role screen
 - Hide screens based on access, test candidates can't view company pages (use firstore auth)
 - flatfile - the elegant import button
 - use isAuthed for authenticating both user & role for the respective route - or pass role as a decision variable
 - Do we need cloud function to getCandidate
-- move auth from cloud functions to native functions 
+- move auth from cloud functions to native functions
 - add modal to view candidate as quick view and then forward to detail page & view opps
 - anbody can see anyone's opps, put an auth rule on the pages or on firestore or use https://firebase.google.com/docs/auth/admin/custom-claims
 - marshmallow UI
--- src/opportunities/OpportunityDetailPage.js
+++ src/opportunities/OpportunityDetailPage.js
@@ -1,42 +1,42 @@
-import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
-import { useParams } from 'react-router-dom';
-import { 
-    Button,
-    Heading,
-    Link,
-    MaxWidthContentSection,
-    Modal,
-    Rating,
-    SelectionList,
-    Tag,
-    Thumbnail,
-} from '../ui';
-import { ReviewListItem, getReviews } from '../reviews';
-import { getOpportunityDetail } from '../opportunities';
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import { useParams } from "react-router-dom";
+import {
+  Button,
+  Heading,
+  Link,
+  MaxWidthContentSection,
+  Modal,
+  Rating,
+  SelectionList,
+  Tag,
+  Thumbnail,
+} from "../ui";
+import { ReviewListItem, getReviews } from "../reviews";
+import { getOpportunityDetail } from "../opportunities";
 const ThumbnailWrap = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 const DetailsSection = styled.div`
-    flex: 8;
+  flex: 8;
 `;
 
 const TagSection = styled.div`
-    margin: 16px 0;
+  margin: 16px 0;
 `;
 
 const FullWidthButton = styled(Button)`
-    margin-top: 16px;
-    display: block;
-    width: 100%;
+  margin-top: 16px;
+  display: block;
+  width: 100%;
 `;
 
 const CenteredButton = styled(Button)`
-    display: block;
-    margin: auto;
+  display: block;
+  margin: auto;
 `;
 
 /*
@@ -45,65 +45,72 @@ const CenteredButton = styled(Button)`
     button that displays a modal of available reservation times.
 */
 export const OpportunityDetailPage = () => {
-    const [isLoading, setIsLoading] = useState(true);
-    const [opportunity, setOpportunityDetail] = useState({}); //state for the candidate
-    const [reviews, setReviews] = useState([]); //state for the reviews
-    const { title,
-        organisation,
-        location,
-        type,
-        description,
-        qualification,
-        howToApply,
-        deadline, tags = []} = opportunity || {};
+  const [isLoading, setIsLoading] = useState(true);
+  const [opportunity, setOpportunityDetail] = useState({}); //state for the candidate
+  const [reviews, setReviews] = useState([]); //state for the reviews
+  const {
+    title,
+    organisation,
+    location,
+    type,
+    description,
+    qualification,
+    howToApply,
+    deadline,
+    tags = [],
+  } = opportunity || {};
 
-    const { id } = useParams();
-    const [modalIsOpen, setModalIsOpen] = useState(false);
+  const { id } = useParams();
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
-    useEffect(() => {
-        // Firebase code for loading the candidate goes here
-        const loadOpportunityDetail = async () => {
-            const result = await getOpportunityDetail(id);
-            setOpportunityDetail(result);
-            setIsLoading(false);
-        }
-        loadOpportunityDetail();
-    }, [id]);
+  useEffect(() => {
+    // Firebase code for loading the candidate goes here
+    const loadOpportunityDetail = async () => {
+      const result = await getOpportunityDetail(id);
+      setOpportunityDetail(result);
+      setIsLoading(false);
+    };
+    loadOpportunityDetail();
+  }, [id]);
 
-    useEffect(() => {
-        // Firebase code for loading the candidate's reviews goes here
-        const loadReviews = async () => {
-            const result = await getReviews(id);
-            setReviews(result);
-        }
-        loadReviews();
-    }, [id]);
+  useEffect(() => {
+    // Firebase code for loading the candidate's reviews goes here
+    const loadReviews = async () => {
+      const result = await getReviews(id);
+      setReviews(result);
+    };
+    loadReviews();
+  }, [id]);
 
-    // Display a loading message while the Firebase data is loading
-    return isLoading ? <Heading>Loading...</Heading> : (
-        <MaxWidthContentSection>
-            <Heading>{title} {organisation}</Heading>
-            <ThumbnailWrap>
-                <Thumbnail height='300px' width='600px' url={opportunity.imageUrl} />
-            </ThumbnailWrap>
-            <DetailsSection>
-                <div>
-                    {location},&nbsp;
-                    {type},&nbsp;
-                    {description},&nbsp;
-                    {qualification},&nbsp;
-                    {howToApply},&nbsp;
-                    {deadline}&nbsp;
-                </div>
-                <TagSection>
-                    {/* {tags.map(tag => <Tag key={tag}>{tag}</Tag>)} */}
-                </TagSection>
-                <div>{description}</div>
-            </DetailsSection>
-            <Modal
-                isOpen={modalIsOpen}
-                onRequestClose={() => setModalIsOpen(false)}>
-            </Modal>
-        </MaxWidthContentSection>
-    )
-}
+  // Display a loading message while the Firebase data is loading
+  return isLoading ? (
+    <Heading>Loading...</Heading>
+  ) : (
+    <MaxWidthContentSection>
+      <Heading>
+        {title} {organisation}
+      </Heading>
+      <ThumbnailWrap>
+        <Thumbnail height='300px' width='600px' url={opportunity.imageUrl} />
+      </ThumbnailWrap>
+      <DetailsSection>
+        <div>
+          {location},&nbsp;
+          {type},&nbsp;
+          {description},&nbsp;
+          {qualification},&nbsp;
+          {howToApply},&nbsp;
+          {deadline}&nbsp;
+        </div>
+        <TagSection>
+          {/* {tags.map(tag => <Tag key={tag}>{tag}</Tag>)} */}
+        </TagSection>
+        <div>{description}</div>
+      </DetailsSection>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={() => setModalIsOpen(false)}
+      ></Modal>
+    </MaxWidthContentSection>
+  );
+};
